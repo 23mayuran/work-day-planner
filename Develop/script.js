3cd8ba346fc9e263fc02cd8c49b3bcc2e78f6a2e
Athimoolam <maxat@amazon.com>
2020-04-25 19:13:24 -0700

    9 through 5 added

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,36 @@ let beforeTime = moment().startOf('day').add(9, "hours");
 time1 = beforeTime.add(0, "h");
 time1 = time1.format('hh:mm A');
 $(".block1").text(time1);
+
+
+let time2 = beforeTime.add(1, "h");
+time2 = time2.format('hh:mm A');
+$(".block2").text(time2);
+
+let time3 = beforeTime.add(1, "h");
+time3 = time3.format('hh:mm A');
+$(".block3").text(time3);
+
+let time4 = beforeTime.add(1, "h");
+time4 = time4.format('hh:mm A');
+$(".block4").text(time4);
+
+let time5 = beforeTime.add(1, "h");
+time5 = time5.format('hh:mm A');
+$(".block5").text(time5);
+
+let time6 = beforeTime.add(1, "h");
+time6 = time6.format('hh:mm A');
+$(".block6").text(time6);
+
+let time7 = beforeTime.add(1, "h");
+time7 = time7.format('hh:mm A');
+$(".block7").text(time7);
+
+let time8 = beforeTime.add(1, "h");
+time8 = time8.format('hh:mm A');
+$(".block8").text(time8);
+
+let time9 = beforeTime.add(1, "h");
+time9 = time9.format('hh:mm A');
+$(".block9").text(time9);
